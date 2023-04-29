61e685055a159658c5b2d9db2bfae153d696860b
Vukasin <vule2002.vc@gmail.com>
2023-04-29 16:40:38 +0200

    Added the initial activities and fragments

---
+++ app/src/main/java/com/example/ma_projekat/menuFragments/FriendsFragment.java
@@ -0,0 +1,28 @@
+package com.example.ma_projekat.menuFragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ma_projekat.R;
+
+
+public class FriendsFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friends, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/ma_projekat/menuFragments/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.example.ma_projekat.menuFragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ma_projekat.R;
+
+public class HomeFragment extends Fragment {
+
+    View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_main_menu, container, false);
+        return view;
+    }
+}
