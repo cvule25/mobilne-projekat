249f22abd1748e56d95b21da778a924e3b5824e8
cvule25 <vule2002.vc@gmail.com>
2023-05-01 01:29:28 +0100

    Implemented Header and navigation for all of the games

--- app/src/main/java/com/example/ma_projekat/gameFragments/KoznaznaFragment.java
+++ app/src/main/java/com/example/ma_projekat/gameFragments/KoznaznaFragment.java
@@ -2,20 +2,46 @@ package com.example.ma_projekat.gameFragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.ma_projekat.R;
 
 public class KoznaznaFragment extends Fragment {
 
+
+    Button nextGame;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_koznazna, container, false);
+        View view = inflater.inflate(R.layout.fragment_koznazna, container, false);
+        Button nextButton = (Button) view.findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment nextGame = new AsocijacijeFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameLayout2, nextGame);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+        return view;
     }
+
 }
